4d57a3ef82b426a03785311e8748b5d161964e17
tortangChris <eliseriochrisashley@gmail.com>
2025-02-03 21:13:47 +0800

    Add CabuTrans Logo

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <nav className="bg-black p-2 sm:p-5 md:p-5 lg:p-3">
-      <div className="container mx-auto flex justify-between items-start">
-        <h1 className="text-white text-lg sm:text-lg md:text-xl lg:text-2xl font-semibold mr-auto">
+    <nav className="bg-black p-2 sm:p-6 md:p-6 lg:p-1">
+      <div className="container mx-auto flex justify-between items-center">
+        <img
+          src="/CabuTrans.png"
+          alt="CabuTrans Logo"
+          className="h-8 sm:h-8 md:h-10 lg:h-16"
+        />
+
+        <h1 className="text-white text-lg sm:text-m md:text-lg lg:text-xl font-semibold mr-auto">
           CabuTrans
         </h1>
       </div>
