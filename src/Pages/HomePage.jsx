002d5030c2e3688f3542ebbe2e4f44aad8bbd2ca
tortangChris <eliseriochrisashley@gmail.com>
2025-02-05 20:15:17 +0800

    Added content for homepage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,8 +2,34 @@ import React from "react";
 
 const HomePage = () => {
   return (
-    <div className="Container bg-amber-50">
-      <h1>Home Page</h1>
+    <div>
+      <div className="container p-4 mx-auto">
+        <h1 className="text-3xl font-bold">
+          Navigate around Cabuyao with ease
+        </h1>
+        <h1 className="text-2xl text-gray-500">
+          Using the best transporation options
+        </h1>
+      </div>
+
+      <div className="container p-4 mx-auto">
+        <h1 className="text-2xl font-bold">
+          Whether you're traveling by tricycle or walking, we'll guide you to
+          the best routes.
+        </h1>
+
+        <h1>
+          Get real-time transport updates and simplified navigation for
+          students.
+        </h1>
+      </div>
+
+      <div className="container p-4 mx-auto">
+        <h1 className="text-2xl font-bold">Why Choose CabuTrans?</h1>
+        <h1>Easy-to-read maps</h1>
+        <h1>Real-Time transport updates</h1>
+        <h1>Simplified navigation for students and visitors</h1>
+      </div>
     </div>
   );
 };
