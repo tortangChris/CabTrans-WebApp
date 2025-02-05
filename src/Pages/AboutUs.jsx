ee6199082f464651776102afac49b2b5b98bf0fc
ellegragasin <rochellevalenzuela15@gmail.com>
2025-02-05 23:11:41 +0800

    Added description for About The App content

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const About = () => {
   return (
     <div className="min-h-screen flex flex-col items-center p-6 bg-[#D9D9D9]">
-      <div className="max-w-5xl w-full bg-[#FFFFFF] shadow-lg rounded-xl p-6 sm:p-10 text-center"></div>
+      <div className="max-w-5xl w-full bg-[#FFFFFF] shadow-lg rounded-xl p-6 sm:p-10 text-center">
+           <h2 className="text-3xl font-bold text-gray-800">About The App</h2>
+        <p className="mt-4 text-gray-700 text-justify">
+          CabuTrans is a smart transportation app designed to make your daily commute easier, faster, and more efficient. It provides clear and easy-to-follow routes, highlighting major stops and the quickest way to your destination. You can stay up-to-date with real-time transport information, including estimated arrival times and the current status of trips. Whether you're a student heading to school or a visitor exploring the city, CabuTrans offers tailored routes and directions to meet your needs. With user-friendly maps, live updates, and a seamless experience, getting around the city has never been more convenient.
+        </p>
+        </div>
 
       <div className="max-w-5xl w-full bg-[#FFFFFF] shadow-lg rounded-xl p-6 sm:p-10 mt-8 text-center">
       
