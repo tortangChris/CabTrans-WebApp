53ac110d3d35a50d6bdf1dc148d701592ef787a7
ellegragasin <rochellevalenzuela15@gmail.com>
2025-02-06 08:13:42 +0800

    Updated AboutUs Page

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -49,14 +49,11 @@ const About = () => {
             </div>
           ))}
         </div>
-
-      </div>
-
-      <div className="max-w-5xl w-full bg-[#FFFFFF] shadow-lg rounded-xl p-6 sm:p-10 mt-8 text-center">
-        <p className="text-gray-700 text-justify">
+        <p className="text-gray-700 mt-10">
           CabuTrans was developed by a dedicated team of 3rd-year BS Computer Science students from Pamantasan ng Cabuyao, aiming to improve the city's transportation system. Combining their skills in frontend and backend development, they created a user-friendly, efficient, and reliable mobile application.
         </p>
       </div>
+
     </div>
   );
 };
