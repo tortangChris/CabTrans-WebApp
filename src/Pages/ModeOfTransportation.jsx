fc31f11894f262dc24c6b0fdc33e44042a67aaaf
tortangChris <eliseriochrisashley@gmail.com>
2025-02-11 21:12:57 +0800

    Enhance mode of trans v2

--- src/Pages/ModeOfTransportation.jsx
+++ src/Pages/ModeOfTransportation.jsx
@@ -38,6 +38,13 @@ const ModeOfTransportation = () => {
       </div>
 
       <div className="container mx-auto bg-white shadow-lg rounded-lg p-6 flex flex-col items-center justify-between space-y-4 flex-grow">
+        <div className="w-full bg-gray-800 text-white p-4 rounded-lg flex items-center">
+          <Info size={24} className="mr-2" />
+          <span className="text-sm sm:text-base">
+            Choose one for your preferred route
+          </span>
+        </div>
+
         <button
           className={`w-full sm:w-72 md:w-96 lg:w-180 h-40 rounded-lg text-white font-bold text-center flex flex-col items-center justify-center space-y-2 ${
             selectedMode === "Walking" ? "bg-black" : "bg-gray-400"
@@ -57,13 +64,6 @@ const ModeOfTransportation = () => {
           <img src="/Tricycle.png" alt="Tricycle" className="w-26 h-16" />
           <span>Tricycle</span>
         </button>
-
-        <div className="bg-gray-800 text-white p-4 rounded-lg flex items-center">
-          <Info size={24} className="mr-2" />
-          <span className="text-sm sm:text-base">
-            Choice 1: Your selected route
-          </span>
-        </div>
 
         <div className="w-full flex justify-center mt-auto">
           <button
